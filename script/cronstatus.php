78dd2ec85f975eff48ddc84d00a860407481b030
Broken Arrow <SKJoy2001@GMail.Com>
2020-09-27 08:52:09 +0600

    Update

--- script/cronstatus.php
+++ script/cronstatus.php
@@ -1,120 +1,126 @@
 <?php
 namespace sPHP;
+$CronHTML = [];
 
 foreach([
 	"Application",
+	//"Non critical", 
 ] as $CronIndex => $Cron){
-	$StatusJSONFile = "{$Environment->ScriptPath()}cron/process/{$Cron}/status.json";
-	$Status = json_decode(file_get_contents($StatusJSONFile));
-	$CronIsRunning = $Status->Running ? "Yes" : "No";
+	$CronFileBaseName = str_replace(" ", "_", $Cron);
+	$StatusJSONFile = "{$Environment->ScriptPath()}cron/process/{$CronFileBaseName}/status.json";
 
-	$CronHTML[] = "
-		<h2 id=\"Cron_{$CronIndex}\">
-			<img src=\"{$Environment->IconURL()}process.png\" alt=\"Process\" class=\"Icon\">
-			{$Cron}
+	if(file_exists($StatusJSONFile)){
+		$Status = json_decode(file_get_contents($StatusJSONFile));
+		$CronIsRunning = $Status->Running ? "Yes" : "No";
 
-			<span class=\"Action\">
-				<a href=\"{$Application->URL($_POST["_Script"], "RandomNumber=" . rand() . "")}#Cron_{$CronIndex}\" class=\"Item\"><img src=\"{$Environment->IconURL()}reload.png\" alt=\"Reload\" title=\"Reload\" class=\"Icon\">Reload</a>
-				<a href=\"{$Application->URL("Cron/{$Cron}", "Execute")}\" target=\"_blank\" class=\"Item\"><img src=\"{$Environment->IconURL()}execute.png\" alt=\"Execute\" title=\"Execute\" class=\"Icon\">Execute</a>
-				<a href=\"{$Application->URL("Cron/{$Cron}", "SetResume")}\" target=\"_blank\" class=\"Item\"><img src=\"{$Environment->IconURL()}start.png\" alt=\"Start\" title=\"Start\" class=\"Icon\">Start</a>
-				<a href=\"{$Application->URL("Cron/{$Cron}", "SetExit")}\" target=\"_blank\" class=\"Item\"><img src=\"{$Environment->IconURL()}stop.png\" alt=\"Stop\" title=\"Stop\" class=\"Icon\">Stop</a>
-			</span>
-		</h2>
+		$CronHTML[] = "
+			<h2 id=\"Cron_{$CronIndex}\">
+				<img src=\"{$Environment->IconURL()}process.png\" alt=\"Process\" class=\"Icon\">
+				{$Cron}
 
-		<table class=\"CronTable\">
-			<thead>
+				<span class=\"Action\">
+					<a href=\"{$Application->URL($_POST["_Script"], "RandomNumber=" . rand() . "")}#Cron_{$CronIndex}\" class=\"Item\"><img src=\"{$Environment->IconURL()}reload.png\" alt=\"Reload\" title=\"Reload\" class=\"Icon\">Reload</a>
+					<a href=\"{$Application->URL("Cron/{$CronFileBaseName}", "Command=EXECUTE")}\" target=\"_blank\" class=\"Item\"><img src=\"{$Environment->IconURL()}execute.png\" alt=\"Execute\" title=\"Execute\" class=\"Icon\">Execute</a>
+					<a href=\"{$Application->URL("Cron/{$CronFileBaseName}", "Command=RESUME")}\" target=\"_blank\" class=\"Item\"><img src=\"{$Environment->IconURL()}start.png\" alt=\"Start\" title=\"Start\" class=\"Icon\">Start</a>
+					<a href=\"{$Application->URL("Cron/{$CronFileBaseName}", "Command=EXIT")}\" target=\"_blank\" class=\"Item\"><img src=\"{$Environment->IconURL()}stop.png\" alt=\"Stop\" title=\"Stop\" class=\"Icon\">Stop</a>
+				</span>
+			</h2>
+
+			<table class=\"CronTable\">
+				<thead>
+					<tr>
+						<th rowspan=\"2\">Running</th>
+						<th colspan=\"3\">Time</th>
+						<th colspan=\"2\">Load</th>
+						<th colspan=\"2\">Exit</th>
+						<th colspan=\"2\">Configuration</th>
+						<th colspan=\"4\">Iteration</th>
+						<th rowspan=\"2\">Status</th>
+					</tr>
+					<tr>
+						<th>Begin</th>
+						<th>End</th>
+						<th>Duration</th>
+						<th>Memory</th>
+						<th>CPU</th>
+						<th>Reason</th>
+						<th>Time</th>
+						<th>Interval</th>
+						<th>Time limit</th>
+						<th>Count</th>
+						<th>Begin</th>
+						<th>End</th>
+						<th>Duration</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class=\"HighlightPositive_{$CronIsRunning}\">{$CronIsRunning}</td>
+						<td>" . date("Y-m-d H:i:s", strtotime($Status->Time->Begin)) . "</td>
+						<td>" . date("Y-m-d H:i:s", strtotime($Status->Time->End)) . "</td>
+						<td class=\"HighlightImportant\">{$Utility->SecondToTime($Status->Time->Duration)}</td>
+						<td class=\"HighlightWarning\">" . round($Status->Load->Memory / 1024 / 1024, 2) . " MB</td>
+						<td class=\"HighlightWarning\">" . number_format($Status->Load->System, 2) . "%</td>
+						<td>{$Status->Exit->Reason}</td>
+						<td>" . ($Status->Exit->Time ? date("Y-m-d H:i:s", strtotime($Status->Exit->Time)) : null) . "</td>
+						<td>{$Utility->SecondToTime($Status->Configuration->Interval)}</td>
+						<td>{$Utility->SecondToTime($Status->Configuration->MaximumExecutionTime)}</td>
+						<td class=\"HighlightImportant2\">{$Status->Iteration->Count}</td>
+						<td>" . date("Y-m-d H:i:s", strtotime($Status->Iteration->Time->Begin)) . "</td>
+						<td>" . date("Y-m-d H:i:s", strtotime($Status->Iteration->Time->End)) . "</td>
+						<td class=\"HighlightImportant\">{$Utility->SecondToTime($Status->Iteration->Time->Duration)}</td>
+						<td class=\"HighlightImportant\">{$Utility->SecondToTime(time() - filemtime($StatusJSONFile))}</td>
+					</tr>
+				</tbody>
+			</table>
+		";
+	//DebugDump($Status);
+		$JobTableRowHTML = [];
+		$JobCount = 0;
+
+		foreach($Status->Job as $JobName => $Job){ //DebugDump($Job);
+			$JobCount++;
+
+			$JobTableRowHTML[$Job->Configuration->Order] = "
 				<tr>
-					<th rowspan=\"2\">Running</th>
-					<th colspan=\"3\">Time</th>
-					<th colspan=\"2\">Load</th>
-					<th colspan=\"2\">Exit</th>
-					<th colspan=\"2\">Configuration</th>
-					<th colspan=\"4\">Iteration</th>
-					<th rowspan=\"2\">Status</th>
+					<td class=\"Serial\">{$JobCount}</td>
+					<td>
+						<span class=\"Title\">{$JobName}</span><br>
+						<span class=\"Command\">{$Job->Configuration->Command}</span>
+					</td>
+					<td>
+						Interval: {$Utility->SecondToTime($Job->Configuration->Interval)}<br>
+						Limit: {$Utility->SecondToTime($Job->Configuration->MaximumExecutionTime)}
+					</td>
+					<td>
+						Begin: <span class=\"HighlightImportant\">" . date("Y-m-d H:i:s", strtotime($Job->Time->Begin)) . "</span><br>
+						End: " . date("Y-m-d H:i:s", strtotime($Job->Time->End)) . "<br>
+						Duration: {$Utility->SecondToTime($Job->Time->Duration)}
+					</td>
+					<td>" . implode("<br>", array_filter([$Job->Comment, $Job->Result->Error->Message ? "Error: <span class=\"HighlightError\">{$Job->Result->Error->Message}</span>" : null, ])) . "</td>
+					<td>" . implode("<br>", $Job->Result->Status) . "</td>
 				</tr>
-				<tr>
-					<th>Begin</th>
-					<th>End</th>
-					<th>Duration</th>
-					<th>Memory</th>
-					<th>CPU</th>
-					<th>Reason</th>
-					<th>Time</th>
-					<th>Interval</th>
-					<th>Time limit</th>
-					<th>Count</th>
-					<th>Begin</th>
-					<th>End</th>
-					<th>Duration</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td class=\"HighlightPositive_{$CronIsRunning}\">{$CronIsRunning}</td>
-					<td>" . date("Y-m-d H:i:s", strtotime($Status->Time->Begin)) . "</td>
-					<td>" . date("Y-m-d H:i:s", strtotime($Status->Time->End)) . "</td>
-					<td class=\"HighlightImportant\">{$Utility->SecondToTime($Status->Time->Duration)}</td>
-					<td class=\"HighlightWarning\">" . round($Status->Load->Memory / 1024 / 1024, 2) . " MB</td>
-					<td class=\"HighlightWarning\">" . number_format($Status->Load->System, 2) . "%</td>
-					<td>{$Status->Exit->Reason}</td>
-					<td>" . ($Status->Exit->Time ? date("Y-m-d H:i:s", strtotime($Status->Exit->Time)) : null) . "</td>
-					<td>{$Utility->SecondToTime($Status->Configuration->Interval)}</td>
-					<td>{$Utility->SecondToTime($Status->Configuration->MaximumExecutionTime)}</td>
-					<td class=\"HighlightImportant2\">{$Status->Iteration->Count}</td>
-					<td>" . date("Y-m-d H:i:s", strtotime($Status->Iteration->Time->Begin)) . "</td>
-					<td>" . date("Y-m-d H:i:s", strtotime($Status->Iteration->Time->End)) . "</td>
-					<td class=\"HighlightImportant\">{$Utility->SecondToTime($Status->Iteration->Time->Duration)}</td>
-					<td class=\"HighlightImportant\">{$Utility->SecondToTime(time() - filemtime($StatusJSONFile))}</td>
-				</tr>
-			</tbody>
-		</table>
-	";
-//DebugDump($Status);
-	$JobTableRowHTML = [];
-	$JobCount = 0;
+			";
+		}
 
-	foreach($Status->Job as $JobName => $Job){ //DebugDump($Job);
-		$JobCount++;
+		ksort($JobTableRowHTML);
 
-		$JobTableRowHTML[$Job->Configuration->Order] = "
-			<tr>
-				<td class=\"Serial\">{$JobCount}</td>
-				<td>
-					<span class=\"Title\">{$JobName}</span><br>
-					<span class=\"Command\">{$Job->Configuration->Command}</span>
-				</td>
-				<td>
-					Interval: {$Utility->SecondToTime($Job->Configuration->Interval)}<br>
-					Limit: {$Utility->SecondToTime($Job->Configuration->MaximumExecutionTime)}
-				</td>
-				<td>
-					Begin: <span class=\"HighlightImportant\">" . date("Y-m-d H:i:s", strtotime($Job->Time->Begin)) . "</span><br>
-					End: " . date("Y-m-d H:i:s", strtotime($Job->Time->End)) . "<br>
-					Duration: {$Utility->SecondToTime($Job->Time->Duration)}
-				</td>
-				<td>" . implode("<br>", array_filter([$Job->Comment, $Job->Result->Error->Message ? "Error: <span class=\"HighlightError\">{$Job->Result->Error->Message}</span>" : null, ])) . "</td>
-				<td>" . implode("<br>", $Job->Result->Status) . "</td>
-			</tr>
+		$CronHTML[] = "
+			<table class=\"JobTable\">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Job / Command</th>
+						<th>Configuration</th>
+						<th>Time</th>
+						<th>Comment / Error</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>" . implode(null, $JobTableRowHTML) . "</tbody>
+			</table>
 		";
 	}
-
-	ksort($JobTableRowHTML);
-
-	$CronHTML[] = "
-		<table class=\"JobTable\">
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Job / Command</th>
-					<th>Configuration</th>
-					<th>Time</th>
-					<th>Comment / Error</th>
-					<th>Status</th>
-				</tr>
-			</thead>
-			<tbody>" . implode(null, $JobTableRowHTML) . "</tbody>
-		</table>
-	";
 }
 ?>
 
