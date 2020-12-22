c6e4231b78a1f919c8336b95909a56126a6dc5e6
Broken Arrow <SKJoy2001@GMail.Com>
2020-12-23 00:07:55 +0600

    GIT pull

--- script/utility/git/pull.php
+++ script/utility/git/pull.php
@@ -1,14 +1,26 @@
 <?php
 namespace sPHP;
 
+$Path = substr($Environment->Path(), 0, strlen($Environment->Path()) - 1);
 $Command = "git pull > /dev/null &";
 $Output = $Status = null;
+$TimeBegin = microtime(true);
 
+chdir($Path); // Remove the trailing slash
 exec($Command, $Output, $Status);
 
+$TimeEnd = microtime(true);
+print "<h1>GIT pull: Update from GIT repository</h1>";
+
 DebugDump([
+	"Path" => $Path, 
 	"Command" => $Command, 
 	"Output" => $Output, 
 	"Status" => $Status, 
+	"Time" => [
+		"Begin" => date("r", $TimeBegin), 
+		"End" => date("r", $TimeEnd), 
+		"Duration" => date("H:i:s", $TimeEnd - $TimeBegin), 
+	], 
 ]);
 ?>
