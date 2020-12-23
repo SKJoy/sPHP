cda3ed95f3b54c37ddf5396eb7b4b4539d7cf106
Broken Arrow <SKJoy2001@GMail.Com>
2020-12-23 14:40:12 +0600

    Added GIT pull utility script

--- script/utility/git/pull.php
+++ script/utility/git/pull.php
@@ -8,8 +8,10 @@ $Output = $Status = null;
 
 chdir($Path); // Remove the trailing slash
 exec($Command, $Output, $Status);
+//$Output = shell_exec($Command);
+//system($Command, $Status);
 
-print "<h1>GIT pull: Update from GIT repository</h1>";
+print "<h1><img src=\"{$Environment->IconURL()}git_pull.png\" alt=\"Cron\" class=\"Icon\"> GIT pull: Update from GIT repository</h1>";
 $TimeEnd = microtime(true);
 
 DebugDump([
