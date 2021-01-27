0dae40b44f60dde8b9418dd48f933a8c7f908de0
Broken Arrow <SKJoy2001@GMail.Com>
2021-01-27 23:51:46 +0600

    Update

--- script/utility/phperrorlog.php
+++ script/utility/phperrorlog.php
@@ -3,24 +3,37 @@ namespace sPHP;
 $ErrorLogFile = "{$Environment->Path()}error.php.log";
 $ErrorLogFileExists = file_exists($ErrorLogFile);
 
-if($ErrorLogFileExists && isset($_POST["btnClear"])){
-	unlink($ErrorLogFile);
-	$ErrorLogFileExists = file_exists($ErrorLogFile);
-	//print HTML\UI\MessageBox("PHP error log cleared.", "System");
-}
-?>
+if($ErrorLogFileExists){
+	$ErrorLogFileModificationTimeCaption = date("{$Configuration["ShortDateFormat"]} {$Configuration["TimeFormat"]}", filemtime($ErrorLogFile));
 
-<h1>PHP error log</h1>
+	if(isset($_POST["btnClear"])){
+		unlink($ErrorLogFile);
+		$ErrorLogFileExists = file_exists($ErrorLogFile);
+		//print HTML\UI\MessageBox("PHP error log cleared.", "System");
+	}
+}
+else{
+	$ErrorLogFileModificationTimeCaption = "Never";
+}
+
+$ErrorLogContentElementID = "ErrorLogContent_" . rand() . "";
+?>
 
 <div class="Console">
 	<div class="Title">
-		PHP error log
+		PHP error log<?=$ErrorLogFileModificationTimeCaption == "Never" ? null : " : {$ErrorLogFileModificationTimeCaption}"?>
 
 		<nav class="Action">
+			<a href="<?=$Application->URL($_POST["_Script"], "")?>" class="Item" onclick="return true;"><img src="<?=$Environment->IconURL()?>refresh.png" alt="Clear" class="Icon">Reload</a>
 			<a href="<?=$Application->URL($_POST["_Script"], "btnClear")?>" class="Item" onclick="return confirm('Are you sure to clear the PHP error log?');"><img src="<?=$Environment->IconURL()?>clear.png" alt="Clear" class="Icon">Clear</a>
 		</nav>
 	</div>
 
-	<div class="Content"><?=$ErrorLogFileExists ? file_get_contents($ErrorLogFile) : null?></div>
-	<div class="Footer">Last error: <?=$ErrorLogFileExists ? date("{$Configuration["ShortDateFormat"]} {$Configuration["TimeFormat"]}", filemtime($ErrorLogFile)) : "Never"?></div>
+	<div id="<?=$ErrorLogContentElementID?>" class="Content"><?=$ErrorLogFileExists ? file_get_contents($ErrorLogFile) : null?></div>
+	<div class="Footer">Last error: <?=$ErrorLogFileModificationTimeCaption?></div>
 </div>
+
+<script>
+	var ErrorLogContentElement = document.getElementById('<?=$ErrorLogContentElementID?>');
+	ErrorLogContentElement.scrollTo(0, ErrorLogContentElement.scrollHeight);
+</script>
