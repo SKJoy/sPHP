75df9367aa20fbbcbf11c54a68dab487a5b536a7
Broken Arrow <SKJoy2001@GMail.Com>
2021-07-07 23:28:40 +0600

    Managed code

--- script/cron/maintenance/database/clean.php
+++ script/cron/maintenance/database/clean.php
@@ -2,64 +2,82 @@
 namespace sPHP;
 
 if(isset($sPHPCronJob)){ // In case if this script runs through sPHP Cron object
-	$Table = $Resource["Table"];
-	$Utility = $Resource["Utility"];
-	$Configuration = $Resource["Configuration"];
+	$Table = \sPHP::$Table;
+	$Utility = \sPHP::$Utility;
+	$Configuration = \sPHP::$Configuration;
 
 	$Database = $Table[array_keys($Table)[0]]->Database();
 }
 
-$CheckpointTime = microtime(true);
+$DateTimeFormat = "{$CFG["DateFormat"]} {$CFG["TimeFormat"]}";
 
 $Recordset = $Database->Query("
-	SET @HistoryMonthsToKeep := 3; # Keep data of past N months
-	SET @TimeToKeepFrom := CONCAT(DATE_ADD(DATE_FORMAT(NOW(), '%Y-%m-01'), INTERVAL (-1) * @HistoryMonthsToKeep MONTH), ' 00:00:00');
-	SET @ProcessTimeStart := NOW();
-
-	SELECT @TimeToKeepFrom AS TimeToKeepFrom;
+	# Parameter
+		SET @HistoryMonthsToKeep := 3; # Keep data of past N months
+		SET @ProcessTimeStart := NOW();
+		SET @TimeToKeepFrom := CONCAT(DATE_ADD(DATE_FORMAT(@ProcessTimeStart, '%Y-%m-01'), INTERVAL (-1) * @HistoryMonthsToKeep MONTH), ' 00:00:00');
 
 	# System: Maintenance: Clean
-	DELETE FROM sphp_notification WHERE TimeInserted < @TimeToKeepFrom LIMIT 9999;
-	DELETE FROM sphp_userdevice WHERE TimeInserted < @TimeToKeepFrom LIMIT 9999;
-	DELETE FROM sphp_useruserdevice WHERE UserUserDeviceTimeActiveLast < @TimeToKeepFrom LIMIT 9999;
-	DELETE FROM sphp_useruserdevicenotification WHERE TimeInserted < @TimeToKeepFrom OR UserUserDeviceNotificationIsRead = 1 LIMIT 9999;
+		DELETE FROM sphp_notification WHERE TimeInserted < @TimeToKeepFrom LIMIT 9999;
+		DELETE FROM sphp_userdevice WHERE TimeInserted < @TimeToKeepFrom LIMIT 9999;
+		DELETE FROM sphp_useruserdevice WHERE UserUserDeviceTimeActiveLast < @TimeToKeepFrom LIMIT 9999;
+		DELETE FROM sphp_useruserdevicenotification WHERE TimeInserted < @TimeToKeepFrom OR UserUserDeviceNotificationIsRead = 1 LIMIT 9999;
 
 	# Clean up aged Application Traffic
-	DELETE FROM		sphp_applicationtraffic
-	WHERE			ApplicationTrafficTime < @TimeToKeepFrom
-		OR			ApplicationTrafficScript LIKE 'cron/%'
-		#OR			ApplicationTrafficScript LIKE 'api/%'
-		#OR			ApplicationTrafficScript IN ('user/signout', 'user/signin', 'user/signinaction', 'home')
-		OR			ApplicationTrafficIP IN (
-						'127.0.0.1', # Local
-						''
-					)
-	LIMIT 9999;
+		DELETE FROM		sphp_applicationtraffic
+		WHERE			ApplicationTrafficTime < @TimeToKeepFrom
+			OR			ApplicationTrafficScript LIKE 'cron/%'
+			#OR			ApplicationTrafficScript LIKE 'api/%'
+			#OR			ApplicationTrafficScript IN ('user/signout', 'user/signin', 'user/signinaction', 'home')
+			OR			ApplicationTrafficIP IN (
+							'127.0.0.1', # Local
+							''
+						)
+		LIMIT 9999;
 
-	SELECT 'Complete' AS Status, @ProcessTimeStart AS Begin, TIMEDIFF(NOW(), @ProcessTimeStart) AS Duration;
+	# Status
+		SELECT			@TimeToKeepFrom AS TimeToKeepFrom, 
+						@ProcessTimeStart AS ProcessTimeStart, 
+						TIMEDIFF(NOW(), @ProcessTimeStart) AS ProcessDuration
+		;
 ");
 
-$CheckpointTime_Database = microtime(true);
-$ProcessStatus = is_array($Recordset) && isset($Recordset[count($Recordset) - 1][0]["Status"]) ? "Complete" : "Failed";
+if(is_array($Recordset) && count($Recordset)){
+	$ProcessStatus = $Recordset[0][0];
+	$TimeToKeepFromCaption = date($DateTimeFormat, strtotime($ProcessStatus["TimeToKeepFrom"]));
+	$ProcessTimeStartCaption = date($DateTimeFormat, strtotime($ProcessStatus["ProcessTimeStart"]));
 
-if(isset($sPHPCronJob)){ // Resturn result if ran through sPHP Cron object
-	$CronJobResult["Error"] = ["Code" => 0, "Message" => null, ];
-
-	$CronJobResult["Status"] = [
-		"Status: {$ProcessStatus}",
-		"Begin: " . date("H:i:s", $CheckpointTime) . "",
-		"End: " . date("H:i:s", $CheckpointTime_Database) . " (" . number_format($CheckpointTime_Database - $CheckpointTime, 3) . ")",
-	];
+	if(isset($sPHPCronJob)){ // Resturn result if ran through sPHP Cron object
+		$CronJobResult["Error"] = ["Code" => 0, "Message" => null, ];	
+	
+		$CronJobResult["Status"] = [
+			"Keep from: {$TimeToKeepFromCaption}",
+		];
+	}
+	else{ // Output only if not an sPHP Cron Job (service, background process)
+		print "
+			<h2>Database: Clean</h2>
+	
+			<ul>
+				<li>Keep from: {$TimeToKeepFromCaption}</li>
+				<li>Begin: {$ProcessTimeStartCaption}</li>
+				<li>Duration: {$ProcessStatus["ProcessDuration"]}</li>
+			</ul>
+		";
+	}
 }
-else{ // Output only if not an sPHP Cron Job (service, background process)
-	print "
-		<h2>Database: Clean</h2>
-
-		<ul>
-			<li>Status: {$ProcessStatus}</li>
-			<li>Begin: " . date("H:i:s", $CheckpointTime) . "</li>
-			<li>End: " . date("H:i:s", $CheckpointTime_Database) . " (" . number_format($CheckpointTime_Database - $CheckpointTime, 3) . ")</li>
-		</ul>
-	";
+else{
+	if(isset($sPHPCronJob)){ // Resturn result if ran through sPHP Cron object
+		$CronJobResult["Error"] = ["Code" => -1, "Message" => "Database query failed", ];
+	}
+	else{ // Output only if not an sPHP Cron Job (service, background process)
+		print "
+			<h2>Database: Clean</h2>
+	
+			<ul>
+				<li>Error: Database query failed</li>
+			</ul>
+		";
+	}
 }
 ?>
